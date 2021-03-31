5a123693496daba3c8c8a3ead6abb03412ba2552
MIHYEON BYEON <66788867+cocoball200@users.noreply.github.com>
2021-03-31 22:08:50 +0900

    Add : scrollspy 구현 수정

---
+++ src/ScrollSpy/Content.js
@@ -0,0 +1,4 @@
+import React, { forwardRef } from "react";
+
+const Content = ({ page }, ref) => <div ref={ref}>{page}</div>;
+export default forwardRef(Content);
--- src/ScrollSpy/IntersectionObserver.js
+++ src/ScrollSpy/IntersectionObserver.js
@@ -1,4 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
+import Nav from "./Nav";
+import Content from "./Content";
 import "./style.scss";
 
 const pages = Array.from({ length: 8 }).map((_, i) => i + 1);
@@ -7,11 +9,43 @@ const IntersectionObserver = () => {
   const [viewIndex, setViewIndex] = useState(0);
   const contentRef = useRef([]);
   const moveToPage = (index) => () => {
-    contentRef.current[index].scrollo;
+    // do something
+    contentRef.current[index].scrollIntoView({
+      block: "start",
+      behavior: "smooth"
+    });
   };
 
-  const scrollSpyObserver = new IntersectionObserver();
-  return <div>hello</div>;
+  const scrollSpyObserver = new IntersectionObserver(
+    (entries) => {
+      // do something
+      const { target } = entries.find((entry) => entry.isIntersecting) || {};
+      setViewIndex(contentRef.current.indexOf(target));
+    },
+    {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.5
+    }
+  );
+
+  useEffect(() => {
+    contentRef.current.forEach((item) => scrollSpyObserver.observe(item));
+    return () => {
+      contentRef.current.forEach((item) => scrollSpyObserver.unobserve(item));
+    };
+  }, []);
+
+  return (
+    <div id="app">
+      <Nav pages={pages} viewIndex={viewIndex} moveToPage={moveToPage} />
+      <div id="contents">
+        {pages.map((p, i) => (
+          <Content key={p} ref={(r) => (contentRef.current[i] = r)} page={p} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default IntersectionObserver;
