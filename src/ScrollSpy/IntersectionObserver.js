f5cb6dc28b7e8204de28b832030d60b6e5c3e447
MIHYEON BYEON <66788867+cocoball200@users.noreply.github.com>
2021-04-01 18:44:59 +0900

    InterscectionObserver 공부 완료

--- src/ScrollSpy/IntersectionObserver.js
+++ src/ScrollSpy/IntersectionObserver.js
@@ -3,13 +3,14 @@ import Nav from "./Nav";
 import Content from "./Content";
 import "./style.scss";
 
-const pages = Array.from({ length: 8 }).map((_, i) => i + 1);
+//const pages = Array.from({ length: 8 }).map((_, i) => i + 1);
+const pages = Array.from({ length: 8 }, (_, i) => i + 1);
 
 const IntersectionObserver = () => {
   const [viewIndex, setViewIndex] = useState(0);
   const contentRef = useRef([]);
   const moveToPage = (index) => () => {
-    // do something
+    // 클릭하면 해당 페이지로 넘어감.
     contentRef.current[index].scrollIntoView({
       block: "start",
       behavior: "smooth"
@@ -18,8 +19,8 @@ const IntersectionObserver = () => {
 
   const scrollSpyObserver = new IntersectionObserver(
     (entries) => {
-      // do something
       const { target } = entries.find((entry) => entry.isIntersecting) || {};
+      // -1은 해당하는 것이 없다.
       setViewIndex(contentRef.current.indexOf(target));
     },
     {
@@ -30,6 +31,7 @@ const IntersectionObserver = () => {
   );
 
   useEffect(() => {
+    //observer 모든 div에게 적용
     contentRef.current.forEach((item) => scrollSpyObserver.observe(item));
     return () => {
       contentRef.current.forEach((item) => scrollSpyObserver.unobserve(item));
@@ -40,9 +42,20 @@ const IntersectionObserver = () => {
     <div id="app">
       <Nav pages={pages} viewIndex={viewIndex} moveToPage={moveToPage} />
       <div id="contents">
-        {pages.map((p, i) => (
-          <Content key={p} ref={(r) => (contentRef.current[i] = r)} page={p} />
-        ))}
+        {pages.map((p, i) => {
+          // console.log(contentRef.current[i]);
+          //스크롤을 내릴때마다, ref.current[i]가 맵을 돌린다.
+          return (
+            <Content
+              key={p}
+              ref={(r) => {
+                console.log(r);
+                contentRef.current[i] = r;
+              }}
+              page={p}
+            />
+          );
+        })}
       </div>
     </div>
   );
